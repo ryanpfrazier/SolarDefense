b4ca453154e9b979558f0326b5072f555a129212
Ryan Frazier <ryanpfrazier@Ryans-MacBook-Pro.local>
2017-06-28 14:57:53 -0600

    styling started

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private float dist;
+	private int waypointIndex;
+
+	void Start () {
+		target = Waypoints.points [0];
+	}
+
+	void Update () {
+		dist = Vector3.Distance (transform.position, target.position);
+		ChangeTarget ();
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+	}
+
+	void ChangeTarget() {
+		if (dist <= 0.25f) {
+			waypointIndex++;
+		}
+		if (waypointIndex >= Waypoints.points.Length) {
+			Destroy (gameObject);
+		}
+		target = Waypoints.points [waypointIndex];
+	}
+}
